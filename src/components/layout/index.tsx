319dc51fc63850c44cfd759444aa56aa2523dfea
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-01-19 04:14:55 +0900

    feat: 로고 적용

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,15 @@
 import { Link, Outlet } from "react-router-dom";
 import Footer from "./footer";
+import logo from "/logo.png";
 
 function Layout() {
   return (
     <>
-      <Link to="/" className="cursor-pointer z-20 sticky top-0 px-12 py-4 text-3xl">
+      <Link
+        to="/"
+        className="cursor-pointer z-20 sticky top-0 px-12 py-4 text-3xl flex items-center gap-2"
+      >
+        <img src={logo} alt="로고" className="w-6 h-5" />
         작심삼냥
       </Link>
       <Outlet />
